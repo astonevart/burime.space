a8ee6d66a647eaff5f67e6c763f8b2838699cf77
Ryabenko Alexandr <astonevart@gmail.com>
2018-10-22 18:29:23 +0300

    New

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready( function(){
         $(".burime-item").each(function () {
-            $(this).css("background-color","hsla("+Math.random()*360+",100%,50%,0.2)")
+            $(this).css("background-color","hsla("+Math.random()*360+",100%,50%,0.1)")
         });
         $(".opened-burime .author").each(function () {
             $(this).css("color","hsla("+Math.random()*360+",100%,50%,0.8)")
